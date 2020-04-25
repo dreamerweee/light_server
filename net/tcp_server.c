c57d5a85461f0124d30eee04781990a6d45c97f9
weee <wtdreamer@163.com>
2020-04-25 20:14:26 +0800

    tcp_server

--- net/tcp_server.c
+++ net/tcp_server.c
@@ -1,11 +1,37 @@
 #include "tcp_server.h"
 
-struct TcpServer* TcpServerInit()
+TcpServer* TcpServerInit(EventLoop *event_loop, Acceptor *acceptor,
+                         ConnectCompletedCallBack connect_completed_cb,
+                         ReadMessageCallBack read_msg_cb,
+                         WriteCompletedCallBack write_completed_cb)
 {
-    
+    TcpServer *tcp_server = (TcpServer *)malloc(sizeof(TcpServer));
+    tcp_server->event_loop = event_loop;
+    tcp_server->acceptor = acceptor;
+    tcp_server->connect_completed_cb = connect_completed_cb;
+    tcp_server->read_msg_cb = read_msg_cb;
+    tcp_server->write_completed_cb = write_completed_cb;
+
+    return tcp_server;
 }
 
-void TcpServerStart(struct TcpServer *tcp_server)
+// 监听套接字可读时的回调处理函数
+static void HandleConnection(void *args)
 {
+    TcpServer *tcp_server = (TcpServer *)args;
+    Acceptor *acceptor = tcp_server->acceptor;
+    int conn_fd = accept(acceptor->listen_fd, NULL, NULL);
+    if (conn_fd < 0) {
+        LogError("HandleConnection accept faileds");
+        return;
+    }
+}
 
+void TcpServerStart(TcpServer *tcp_server)
+{
+    EventLoop *event_loop = tcp_server->event_loop;
+    Acceptor *acceptor = tcp_server->acceptor;
+    // 将 acceptor 作为 channel 加入 event_loop 中
+    Channel *channel = ChannelInit(acceptor->listen_fd, EVENT_READ, HandleConnection, NULL, tcp_server);
+    EventLoopAddChannelEvent(event_loop, channel);
 }
--- net/tcp_server.h
+++ net/tcp_server.h
@@ -1,15 +1,25 @@
 #ifndef TCP_SERVER_H
 #define TCP_SERVER_H
 
+#include "acceptor.h"
 #include "server_event.h"
+#include "server_channel.h"
+#include "tcp_connection.h"
 
-struct TcpServer {
-    int port;
-    struct EventLoop *event_loop;
-};
+typedef struct {
+    EventLoop *event_loop;
+    Acceptor *acceptor;
+    ConnectCompletedCallBack connect_completed_cb;  // 连接完成回调
+    ReadMessageCallBack      read_msg_cb;           // 应用层读取数据的回调
+    WriteCompletedCallBack   write_completed_cb;    // 发送完数据给应用层的回调
+} TcpServer;
 
-struct TcpServer* TcpServerInit();
+TcpServer* TcpServerInit(EventLoop *event_loop, Acceptor *acceptor,
+                         ConnectCompletedCallBack connect_completed_cb,
+                         ReadMessageCallBack read_msg_cb,
+                         WriteCompletedCallBack write_completed_cb);
 
-void TcpServerStart(struct TcpServer *tcp_server);
+// 开始运行
+void TcpServerStart(TcpServer *tcp_server);
 
 #endif  // TCP_SERVER_H
